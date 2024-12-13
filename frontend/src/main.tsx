25d04f49fbeb0e2318a72b9a294c497ff74b373b
Rajitha <rajithapulasthi11@gmail.com>
2024-12-14 00:23:46 +0530

    Basic Nav Bar updated

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import NavBar from "./shared/components/molecules/NavBar";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    
-  </StrictMode>,
-)
+    <NavBar />
+  </StrictMode>
+);
---
+++ frontend/src/shared/components/molecules/NavBar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div className="bg-purple-800 w-full h-[77px] flex items-center justify-center"></div>
+  );
+};
+
+export default NavBar;
