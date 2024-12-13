7bc19b3ff94651326b5a3ab5bee28cb0b6265ef1
Rajitha <rajithapulasthi11@gmail.com>
2024-12-14 03:00:27 +0530

    NavBar Done

--- frontend/src/shared/components/molecules/NavBar.tsx
+++ frontend/src/shared/components/molecules/NavBar.tsx
@@ -3,11 +3,11 @@ import Logo from "../../../assets/Logo.png";
 const NavBar = () => {
   return (
     <div className="bg-primary w-full h-[77px] flex items-center justify-between">
-      <div>
-        <img src={Logo} alt="AT Digital Logo" />
+      <div className="ml-16">
+        <img src={Logo} alt="AT Digital Logo " />
       </div>
       <div>
-        <ul className="flex items-center text-white">
+        <ul className="flex items-center text-white mr-16">
           <a href="">
             <li className="mr-4">SERVICES</li>
           </a>
